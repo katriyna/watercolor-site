93d5862b68bd492b6b27ebd2059b1ed09630aa51
katriyna <katriyna@gmail.com>
2025-12-05 19:14:34 +0100

    about me

--- src/pages/about-me/about-me.js
+++ src/pages/about-me/about-me.js
@@ -23,7 +23,7 @@ function AboutMe() {
             <p>
               In the past I used to be also interested in non-formal conferences, and was co-organiser of <a href="/nc2014" target="_blank">Nërd|Camp 2014</a>, <a href="/nc2015" target="_blank">Nërd|Camp 2015</a>,{' '}
               <a href="/nc2017-spring" target="_blank">Nërd|Camp 2017 Spring</a>, <a href="/nc2017-outumn" target="_blank">Nërd|Camp 2017 Fall</a>,{' '}<a href="/nc2019" target="_blank">Nërd|Camp 2019</a> as well as{' '}
-              conferences at my work.
+              the conferences at my work.
             </p>
           </div>
         </div>
